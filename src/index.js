ea27d11c15583e78aa20dc3c15c12b148e3b5c6b
Ero Stefano <st.mavilio@gmail.com>
2022-08-07 20:20:20 +0200

    add newest draw

--- src/index.js
+++ src/index.js
@@ -22,8 +22,13 @@ sheets = [
     {numbers: ['05', '14', '27', '31', '35', '36'], stars: ['04']}
 ];
 
-draws = [{
-    date: new Date('2022-08-03'), numbers: ['15', '17', '21', '27', '35', '38'], stars: ['06']
-}];
+draws = [
+    {
+        date: new Date('2022-08-03'), numbers: ['15', '17', '21', '27', '35', '38'], stars: ['06']
+    },
+    {
+        date: new Date('2022-08-06'), numbers: ['1', '2', '15', '18', '32', '40'], stars: ['06']
+    }
+];
 
 logger.log('SwissLotto', sheets, draws);
